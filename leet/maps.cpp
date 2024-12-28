049658a03820f8114d7c9a6807c2ba8d868e6176
Jose <95041130+josemicode@users.noreply.github.com>
2024-12-28 10:32:38 +0100

    Added stranger functions (move to bt file)

--- leet/maps.cpp
+++ leet/maps.cpp
@@ -17,3 +17,31 @@ bool canConstruct(string ransomNote, string magazine){
     
     return true;
 }
+
+// TODO: move to another file...
+vector <vector <int> > permute(vector <int> nums) {
+    vector <vector <int> > permutations;
+	btPerm(nums, permutations, 0);
+	return permutations;
+}
+
+void btPerm(vector <int> & nums, vector <vector <int> > & res, int ind) {
+	if(ind == nums.size()) {
+		res.push_back(nums);
+		return;
+	}
+	
+	int i = ind;
+	while(i < nums.size()) {
+		swapper(nums.at(ind), nums.at(i));
+		btPerm(nums, res, ind + 1);
+		swapper(nums.at(ind), nums.at(i));
+		i++;
+	}
+}
+
+void swapper(int & a, int & b) {
+	a = a + b;
+	b = a - b;
+	a = a - b;
+}
